008a9ec00aca20c3af627838ad7257ef8c2a2d00
012503 <xingxiao@htsc.com>
2018-05-11 10:24:53 +0800

    makefile ignore titans

--- deployment/makefile.sh
+++ deployment/makefile.sh
@@ -16,7 +16,7 @@ mkdir rules
 cp /tmp/kafka_2.11-1.1.0.tgz depends/kafka.tgz
 #wget -O depends/flink.tgz http://mirrors.tuna.tsinghua.edu.cn/apache/flink/flink-1.4.2/flink-1.4.2-bin-hadoop28-scala_2.11.tgz
 cp /tmp/flink-1.4.2-bin-hadoop28-scala_2.11.tgz depends/flink.tgz
-wget -O depends/titans.tgz https://github.com/DefenseTeam/titans/archive/${titans_version}.tar.gz
+#wget -O depends/titans.tgz https://github.com/DefenseTeam/titans/archive/${titans_version}.tar.gz
 cp /tmp/TsapCEPEngine-assembly-0.1.jar jars/TsapCepEngine.jar
 cp /tmp/mariadb-10.2.14-linux-x86_64.tar.gz depends/mariadb.tar.gz
 cp /tmp/redis-4.0.9.tar.gz depends/redis.tar.gz
